ce6abed052d755708b8bc581d91737a0339b815b
devrity <39458309+devrity@users.noreply.github.com>
2025-12-06 22:41:18 +0100

    Create GalleryGraphManager.kt

---
+++ app/src/main/java/com/devrity/puzzler/manager/GalleryGraphManager.kt
@@ -0,0 +1,53 @@
+package com.devrity.puzzler.manager
+
+import android.content.Context
+import com.devrity.puzzler.model.GalleryGraph
+import com.devrity.puzzler.model.GalleryNode
+import com.google.gson.Gson
+import java.io.InputStreamReader
+
+class GalleryGraphManager(private val context: Context) {
+    private var galleryGraph: GalleryGraph? = null
+
+    init {
+        loadGalleryGraph()
+    }
+
+    private fun loadGalleryGraph() {
+        try {
+            val inputStream = context.assets.open("gallery_graph.json")
+            val reader = InputStreamReader(inputStream)
+            galleryGraph = Gson().fromJson(reader, GalleryGraph::class.java)
+            reader.close()
+            inputStream.close()
+        } catch (e: Exception) {
+            e.printStackTrace()
+        }
+    }
+
+    fun getGalleryGraph(): GalleryGraph? = galleryGraph
+
+    fun getStartGallery(): GalleryNode? {
+        return galleryGraph?.let { graph ->
+            graph.nodes[graph.startNodeId]
+        }
+    }
+
+    fun getNextGalleries(nodeId: String): List<GalleryNode> {
+        val nextGalleries = mutableListOf<GalleryNode>()
+        galleryGraph?.let { graph ->
+            graph.nodes[nodeId]?.let { currentNode ->
+                currentNode.outgoingEdges.forEach { edgeId ->
+                    graph.nodes.values.find { it.id == edgeId }?.let {
+                        nextGalleries.add(it)
+                    }
+                }
+            }
+        }
+        return nextGalleries
+    }
+
+    fun getGalleryNode(nodeId: String): GalleryNode? {
+        return galleryGraph?.nodes?.get(nodeId)
+    }
+}
